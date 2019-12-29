5cde70d971dcf6629fd06594554448f74ca661a9
Marek Kobyliński <Marecki1121@gmail.com>
2019-12-30 00:05:04 +0100

    add: first version with static commend and direction

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const { execFile } = require('child_process');
+const { exec } = require('child_process');
 console.log(`Starting directory: ${process.cwd()}`);
 process.chdir("C:/Program Files (x86)/VMware/VMware Workstation");
 console.log(`Now im in: ${process.cwd()}`);
-// const child = execFile('', [ls], (error, stdout, stderr) => {
-//   if (error) {
-//     throw error;
-//   }
-//   console.log(stdout);
-// });
+exec('vmrun clone F:/Windows7Prox64/Windows7Prox64.vmx F:/Windows7Prox64(1)/Windows7Prox64(1).vmx linked', (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
+console.log(`Copy 1 Vms done`);
